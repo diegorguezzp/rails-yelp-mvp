6804de9c68bc36046b756971de0b2e66289bc209
Diego Rodriguez Perez <diegorper@gmail.com>
2022-08-17 17:10:14 +0200

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create(name: "Restaurant2", address: "Address2", category: "italian", phone_number: "2222222")
+Restaurant.create(name: "Restaurant3", address: "Address3", category: "japanese", phone_number: "3333333")
+Restaurant.create(name: "Restaurant4", address: "Address4", category: "french", phone_number: "4444444")
+Restaurant.create(name: "Restaurant5", address: "Address5", category: "belgian", phone_number: "5555555")
